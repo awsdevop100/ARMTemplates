41561da6a89c01aa4a0c52895719fe79d76dc815
awsdevop100 <36854752+awsdevop100@users.noreply.github.com>
2018-03-01 20:09:17 +0000

    Update PostWebSiteDeployment.ps1

--- PostWebSiteDeployment.ps1
+++ PostWebSiteDeployment.ps1
@@ -1,31 +1,58 @@
-﻿#Post Website Deployment
+resourceGroupName = "Frontend-RG"
+$NSGGroup = "FrontEnd-NSG"
+$vnetwork = "bupavnet01"
+$Subnet = "frontend1"
+$VMNode0 = "BupafeNode0"
+$VMNode1 = "BupafeNode1"
 
-Remove-AzureRmVMExtension -ResourceGroupName "bupa-lb-webapp02-RG" -ExtensionName "IIS" -VMName "bupaNode0"
-Remove-AzureRmVMExtension -ResourceGroupName "bupa-lb-webapp02-RG" -ExtensionName "AddComputerName" -VMName "bupaNode0"
 
- 
-#Update default.html
-Set-AzureRmVMExtension -ResourceGroupName "bupa-lb-webapp02-RG" `
-    -ExtensionName "AddComputerName" `
-    -VMName "bupaNode0" `
+Remove-AzureRmVMExtension -ResourceGroupName $resourceGroupName -Name "IIS" -VMName  $VMNode0 -force
+Remove-AzureRmVMExtension -ResourceGroupName $resourceGroupName -Name "IIS" -VMName  $VMNode1 -force
+
+
+# Install IIS
+Set-AzureRmVMExtension -ResourceGroupName "$resourceGroupName" `
+    -ExtensionName "IIS" `
+    -VMName $VMNode0 `
     -Location "NorthEurope" `
     -Publisher Microsoft.Compute `
     -ExtensionType CustomScriptExtension `
     -TypeHandlerVersion 1.8 `
-    -SettingString '{"commandToExecute":"powershell Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername)"}'
+    -SettingString '{"commandToExecute":"powershell Install-WindowsFeature Web-Server, Web-Mgmt-Service, Web-Mgmt-Console, Web-Mgmt-Compat, Web-Asp-Net45, Web-App-Dev, NET-Framework-Features; powershell Add-Content -Path \"C:\\inetpub\\wwwroot\\Node.htm\" -Value $($env:computername)"}'
 
-#---
-Remove-AzureRmVMExtension -ResourceGroupName "bupa-lb-webapp02-RG" -ExtensionName "IIS" -VMName "bupaNode1"
-Remove-AzureRmVMExtension -ResourceGroupName "bupa-lb-webapp02-RG" -ExtensionName "AddComputerName" -VMName "bupaNode1"
 
- 
-#Update default.html
-Set-AzureRmVMExtension -ResourceGroupName "bupa-lb-webapp02-RG" `
-    -ExtensionName "AddComputerName" `
-    -VMName "bupaNode1" `
+# Install IIS
+Set-AzureRmVMExtension -ResourceGroupName "$resourceGroupName" `
+    -ExtensionName "IIS" `
+    -VMName $VMNode1 `
     -Location "NorthEurope" `
     -Publisher Microsoft.Compute `
     -ExtensionType CustomScriptExtension `
     -TypeHandlerVersion 1.8 `
-    -SettingString '{"commandToExecute":"powershell Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername)"}'
+    -SettingString '{"commandToExecute":"powershell Install-WindowsFeature Web-Server, Web-Mgmt-Service, Web-Mgmt-Console, Web-Mgmt-Compat, Web-Asp-Net45, Web-App-Dev, NET-Framework-Features; powershell Add-Content -Path \"C:\\inetpub\\wwwroot\\Node.htm\" -Value $($env:computername)"}'
+
+Remove-AzureRmVMExtension -ResourceGroupName $resourceGroupName -Name "IIS" -VMName  $VMNode0 -force
+Remove-AzureRmVMExtension -ResourceGroupName $resourceGroupName -Name "IIS" -VMName  $VMNode1 -force
+
+    
+    #Default.htm - $VMNode0
+Set-AzureRmVMExtension -ResourceGroupName "$resourceGroupName" `
+    -ExtensionName "IIS" `
+    -VMName $VMNode0 `
+    -Location "NorthEurope" `
+    -Publisher Microsoft.Compute `
+    -ExtensionType CustomScriptExtension `
+    -TypeHandlerVersion 1.8 `
+        -SettingString '{"commandToExecute":"powershell Add-Content -Path \"C:\\inetpub\\wwwroot\\Node.html\" -Value $($env:computername)"}'
+
+
+#Default.htm - $VMNode1
+Set-AzureRmVMExtension -ResourceGroupName "$resourceGroupName" `
+    -ExtensionName "IIS" `
+    -VMName $VMNode1 `
+    -Location "NorthEurope" `
+    -Publisher Microsoft.Compute `
+    -ExtensionType CustomScriptExtension `
+    -TypeHandlerVersion 1.8 `
+        -SettingString '{"commandToExecute":"powershell Add-Content -Path \"C:\\inetpub\\wwwroot\\Node.html\" -Value $($env:computername)"}'
 
